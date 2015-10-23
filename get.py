4f8388b6a15eddfe9d3e4b8fd8925496775c0628
Roger Sicart <roger@adserverpub.com>
2015-10-23 16:30:36 +0200

    Get loop.

--- get.py
+++ get.py
@@ -4,25 +4,27 @@ from pystalkd.Beanstalkd import Connection
 import notification
 import pickle
 import base64
+from time import sleep
 
 setup = {
-		'connection': ('localhost', 11300),
-		'tubes': {
-			'watch': ['python'],	
-			'ignore': ['default'],	
-		}
+    'connection': ('localhost', 11300),
+    'tubes': {
+        'watch': ['python'],
+        'ignore': ['default'],
+    },
+    'timeout': 1,
 }
 
 c = Connection(*setup['connection'])
 
 for tube in setup['tubes']['watch']:
-	c.watch(tube)
+    c.watch(tube)
 
 print("Watching tubes {}".format(c.watching()))
 
 
 for tube in setup['tubes']['ignore']:
-	c.ignore(tube)
+    c.ignore(tube)
 
 print("Ignoring tubes {}".format(c.watching()))
 
@@ -30,14 +32,21 @@ print("Ignoring tubes {}".format(c.watching()))
 #	for tube in tubes:
 #		getattr(c, action)(tube)
 
+while True:
 
-job = c.reserve()
+    try:
+        job = c.reserve(timeout=3)
 
-body = job.body.encode('ascii')
-# beanstalk jobs only allows str objects
-thing = base64.b64decode(body)
-notification = pickle.loads(thing)
+        body = job.body.encode('ascii')
+        # beanstalk jobs only allows str objects
+        thing = base64.b64decode(body)
+        notification = pickle.loads(thing)
 
-print(notification)
+        print(notification)
 
-job.delete()
+        job.delete()
+
+    except:
+        pass
+
+    sleep(setup['timeout'])
